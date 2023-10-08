c9172ba662c193353e33b14ea56dda1ce340eb9f
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-08 19:06:56 +0900

    Add grid lines

--- src/BarChart.js
+++ src/BarChart.js
@@ -2,6 +2,7 @@ import { scaleLinear, max, scaleBand } from "d3";
 
 export const BarChart = ({ data, width, height }) => {
   const MARGIN = 10;
+  const lines = [10, 20, 30, 40];
 
   const xScale = scaleLinear()
     .domain([0, max(data, (d) => d.sunshine)])
@@ -22,6 +23,41 @@ export const BarChart = ({ data, width, height }) => {
       stroke="white"
     ></rect>
   ));
-
-  return <svg viewBox={`0 0 ${width} ${height}`}>{rectangles}</svg>;
+  const labels = data.map((d) => (
+    <text
+      textAnchor="end"
+      key={d.city}
+      x={xScale(d.sunshine)}
+      y={yScale(d) + 20}
+      fill="white"
+    >
+      {d.city}
+    </text>
+  ));
+  const gridLines = lines.map((l, i) => (
+    <g key={i}>
+      <line
+        key={i}
+        y1={0}
+        y2={height - MARGIN}
+        x1={xScale(l)}
+        x2={xScale(l)}
+        stroke="white"
+      ></line>
+      <text
+        textAnchor="middle"
+        fontSize={"12px"}
+        x={xScale(l)}
+        y={height - MARGIN}
+      >
+        {l}
+      </text>
+      <text></text>
+    </g>
+  ));
+  return (
+    <svg viewBox={`0 0 ${width} ${height}`}>
+      {rectangles} {gridLines} {labels}
+    </svg>
+  );
 };
