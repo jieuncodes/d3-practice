14d72b2bfaa33abb7e50ae8938bb49858fe07de4
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-08 18:46:00 +0900

    BarChart scale setup
    scaleBand, scaleLinear

--- src/BarChart.js
+++ src/BarChart.js
@@ -1,3 +1,15 @@
+import { scaleLinear, max, scaleBand } from "d3";
+
 export const BarChart = ({ data, width, height }) => {
+  const MARGIN = 10;
+
+  const xScale = scaleLinear()
+    .domain([0, max(data, (d) => d.sunshine)])
+    .range([0, width - MARGIN]);
+
+  const yScale = scaleBand()
+    .domain(data)
+    .range([height - 2 * MARGIN, 0]);
+
   return <svg viewBox={`0 0 ${width} ${height}`}></svg>;
 };
