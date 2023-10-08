e7d1d482dd36a747900806834a00e0694c1fac5e
youredith <110220308+jieuncodes@users.noreply.github.com>
2023-10-08 18:40:15 +0900

    Create BarChart component
    viewBox: resizes the svg element to fit the width and height of the viewport

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BarChart } from "./BarChart";
+import sunshine from "./sunshine.json";
+
 function App() {
-  return <div className="App"></div>;
+  const data = sunshine
+    .map((d) => {
+      return { city: d.CITY, sunshine: d["JUL"] };
+    })
+    .sort((a, b) => b.sunshine - a.sunshine)
+    .slice(0, 20);
+
+  return (
+    <div className="App">
+      <div className="header"></div>
+      <div className="container">
+        <BarChart height={600} width={900} data={data} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/BarChart.js
@@ -0,0 +1,3 @@
+export const BarChart = ({ data, width, height }) => {
+  return <svg viewBox={`0 0 ${width} ${height}`}></svg>;
+};
